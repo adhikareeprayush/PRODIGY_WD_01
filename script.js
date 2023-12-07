74e126f411316aa521dfee0bc46ef8d931b5f95b
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2023-12-07 09:40:42 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
+// Getting references to DOM elements
 let close_icon = document.getElementById("close_icon");
 let open_icon = document.getElementById("list_icon");
 let nav_item = document.getElementsByClassName("nav-item");
 let nav_item_array = Array.from(nav_item);
 
-//opening the menu
+// Opening the menu
 open_icon.addEventListener("click", function(){
+    // Adding the "clicked-menu" class to show the menu
     document.getElementById("nav-links").classList.add("clicked-menu");
+    // Adjusting the display of menu icons
     open_icon.style.display = "none";
     close_icon.style.display = "block";
-    //make the body non-scrollable
+    // Making the body non-scrollable
     document.body.style.overflow = "hidden";
 });
 
-//closing the menu
+// Closing the menu
 close_icon.addEventListener("click", function(){
+    // Removing the "clicked-menu" class to hide the menu
     document.getElementById("nav-links").classList.remove("clicked-menu");
+    // Adjusting the display of menu icons
     open_icon.style.display = "block";
     close_icon.style.display = "none";
-
-    //make the body scrollable again
+    // Making the body scrollable again
     document.body.style.overflow = "scroll";
 });
 
+// Adding click event listeners to navigation items
 nav_item_array.forEach(function(item){
     item.addEventListener("click", function(){
-         // Remove "active" class from all elements with the class "nav-item active"
-         var activeItems = document.querySelectorAll(".nav-item.active");
-         activeItems.forEach(function(activeItem) {
-             activeItem.classList.remove("active");
-         });
- 
-         // Add "active" class to the clicked item
-         item.classList.add("active");
+        // Removing "active" class from all elements with the class "nav-item active"
+        var activeItems = document.querySelectorAll(".nav-item.active");
+        activeItems.forEach(function(activeItem) {
+            activeItem.classList.remove("active");
+        });
+
+        // Adding "active" class to the clicked item
+        item.classList.add("active");
     });
 });
